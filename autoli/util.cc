bd8800a92715a0abacc30e9ea28609850910793c
autoli <auto_lilei_stu@163.com>
2020-12-25 11:58:10 +0800

    fix util bug

---
+++ autoli/util.cc
@@ -0,0 +1,5 @@
+//
+// Created by llaron on 2020/12/24.
+//
+
+#include "util.h"
---
+++ autoli/util.h
@@ -0,0 +1,8 @@
+//
+// Created by llaron on 2020/12/24.
+//
+
+#ifndef LOG_CC_UTIL_H
+#define LOG_CC_UTIL_H
+
+#endif //LOG_CC_UTIL_H
